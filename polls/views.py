33df361010d5851eae4b262c8517af25e7d2f4fc
yosraBouriga <96349221+yosraBouriga@users.noreply.github.com>
2022-01-16 17:50:02 +0100

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+import datetime
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -19,39 +20,24 @@ def show(request, cin):
         else:
             return JsonResponse({'code': 1, 'citoyen': list(data), 'Information': "aucune infraction"}, safe=False)
 
-#Link : http://localhost:8000/polls/insertCitoyen?cin=12345678&nom=Bedhief&prenom=Youssef&email=youssefbedhief@gmail.com
-#Link : http://localhost:8000/polls/insertCitoyen?cin=12356987&nom=Bouriga&prenom=Yosra&email=yosrabouriga@gmail.com
-#Link : http://localhost:8000/polls/insertCitoyen?cin=11111111&nom=Hajji&prenom=Yassine&email=yassinehajji@gmail.com
-#Link : http://localhost:8000/polls/insertCitoyen?cin=98765421&nom=Frioui&prenom=Islem&email=islemfrioui@gmail.com
-
 @csrf_exempt
 def insertCitoyen(request):
-    if (request.method == "GET"):
-        cin=request.GET.get('cin')
-        nom=request.GET.get('nom')
-        prenom=request.GET.get('prenom')
-        email=request.GET.get('email')
-
-        citoyenInserted = Citoyen(cin=int(cin), nom=nom, prenom=prenom, email=email)
-        citoyenInserted.save()
+        citoyens=[{"cin":12345678,"nom":"Bedhief","prenom":"Youssef","email":"youssefbedhief@gmail.com"},
+                  {"cin":12356987,"nom":"Bouriga","prenom":"Yosra","email":"yosrabouriga@gmail.com"},
+                  {"cin":11111111,"nom":"Hajji","prenom":"Yassine","email":"yassinehajji@gmail.com"},
+                  {"cin":98765421,"nom":"Frioui","prenom":"Islem","email":"islemfrioui@gmail.com"}]
+        for c in citoyens:
+            citoyenInserted = Citoyen(cin=c["cin"], nom=c["nom"], prenom=c["prenom"], email=c["email"])
+            citoyenInserted.save()
         return JsonResponse({'code': 1})
-    else:
-        return JsonResponse({'msg': 'no'})
-
-#http://localhost:8000/polls/insertInfraction?cin_citoyen=11111111&date_creation=2021-10-10&montant=60&etat_paiement=1
-#http://localhost:8000/polls/insertInfraction?cin_citoyen=12345678&date_creation=2021-12-29&montant=250&etat_paiement=1
-#http://localhost:8000/polls/insertInfraction?cin_citoyen=12356987&date_creation=2021-10-28&montant=100&etat_paiement=0
 
 @csrf_exempt
 def insertInfraction(request):
-    if (request.method == "GET"):
-        cin = request.GET.get('cin_citoyen')
-        date_creation = request.GET.get('date_creation')
-        montant = request.GET.get('montant')
-        etat_paiement = request.GET.get('etat_paiement')
+    infractions = [{"cin": 12345678, "montant": 250, "etat_paiement": 1},
+                {"cin": 12356987, "montant": 100, "etat_paiement": 1},
+                {"cin": 11111111, "montant": 60, "etat_paiement": 0}]
 
-        infractionInserted = Infraction(cin_citoyen=Citoyen.objects.get(cin=int(cin)), date_creation=date_creation, montant=montant, etat_paiement=etat_paiement)
-        infractionInserted.save()
-        return JsonResponse({'code': 1})
-    else:
-        return JsonResponse({'msg': 'no'})
+    for i in infractions:
+           infractionInserted = Infraction(cin_citoyen=Citoyen.objects.get(cin=i["cin"]), date_creation=datetime.datetime.now(), montant=i["montant"], etat_paiement=i["etat_paiement"])
+           infractionInserted.save()
+    return JsonResponse({'code': 1})
